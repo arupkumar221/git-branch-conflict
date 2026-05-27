0c877e5fd8b0e554107648816b1094e2e03dcd07
arup-kumar <arupkumard31@gmail.com>
2026-05-27 11:11:13 +0530

    insert student

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def search_student():
         print("Student not found.\n")
 
 while True:
-    print("1. Add Student")
+    print("1. insert Student")
     print("2. View Students")
     print("3. Search Student")
     print("4. Exit")
